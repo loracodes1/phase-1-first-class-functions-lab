a9b54501c049df573631bac87d7668ea8b79acd7
florence <loracodes3@gmail.com>
2024-10-09 15:02:41 +0300

    vfyh

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
 // Code your solution in this file!
-const returnFirstTwoDrivers = function(driversArray) {
-    return driversArray.slice(0, 2);
-};
-console.log(returnFirstTwoDrivers(['Antonia', 'Nuru', 'Amari', 'Mo']));
-
-const returnLastTwoDrivers = function(driversArray) {
-    return driversArray.slice(-2);
+const returnFirstTwoDrivers = function() {
+    const driversArray = ['Antonia', 'Nuru', 'Amari', 'Mo']; 
+    return driversArray.slice(0, 2); 
 };
 
-console.log(returnLatTwoDrivers(['Antonia', 'Nuru', 'Amari', 'Mo']));
+console.log(returnFirstTwoDrivers()); 
 
+
+const returnLastTwoDrivers = function() {
+    const driversArray = ['Antonia', 'Nuru', 'Amari', 'Mo'];
+    return driversArray.slice(-2); 
+};
+
+console.log(returnLastTwoDrivers()); 
 
