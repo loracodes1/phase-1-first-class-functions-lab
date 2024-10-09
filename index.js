535fa576334e08542a2035102dfd36bc85305764
florence <loracodes3@gmail.com>
2024-10-09 14:51:25 +0300

    slice function

--- index.js
+++ index.js
@@ -1,4 +1,13 @@
 // Code your solution in this file!
- const returnFirstTwoDrivers = function(driversArray){
-    ['Antonia', 'Nuru', 'Amari', 'Mo']
- }
+const returnFirstTwoDrivers = function(driversArray) {
+    return driversArray.slice(0, 2);
+};
+console.log(returnFirstTwoDrivers(['Antonia', 'Nuru', 'Amari', 'Mo']));
+
+const returnLastTwoDrivers = function(driversArray) {
+    return driversArray.slice(-2);
+};
+
+console.log(returnLatTwoDrivers(['Antonia', 'Nuru', 'Amari', 'Mo']));
+
+
